617778d8752fa7c61f443f02d55e7f5cdd0f1f12
Nabeel-Akhtar-Qbatch <nabeel.akhtar@qbatch.com>
2022-10-06 03:04:22 +0500

    wait condition for US ship from

--- bot.py
+++ bot.py
@@ -49,10 +49,10 @@ def scrape_aliex_data(search):
     saveButton =WAIT.until(EC.presence_of_element_located((By.XPATH, "//button[@class='ui-button ui-button-primary go-contiune-btn']"))).click()
 
 
-    WAIT.until(EC.presence_of_element_located((By.XPATH, "//span[@class='next-input next-medium next-select-inner']")))
-    shipment_From = driver.find_element(By.XPATH, "//span[@class='next-input next-medium next-select-inner']").click()
-    time.sleep(1)
-    shipment_Country = driver.find_element(By.XPATH, "//ul//li[@title='United States']").click()
+    # WAIT.until(EC.presence_of_element_located((By.XPATH, "//span[@class='next-input next-medium next-select-inner']")))
+    shipment_From = WAIT.until(EC.presence_of_element_located((By.XPATH, "//span[@class='next-input next-medium next-select-inner']"))).click()
+    time.sleep(2)
+    shipment_Country = WAIT.until(EC.presence_of_element_located((By.XPATH, "//ul//li[@title='United States']"))).click()
     time.sleep(2)
 
     href_list=[]
